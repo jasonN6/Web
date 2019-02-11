c1ea7286027f535aec4ef74f1ad5376ab0d1c805
D00194632 <D00194632@P1105-PC15.ad.dkit.ie>
2019-02-11 15:08:43 +0000

    1st commit of project

---
+++ model/database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=D00194632';
+    $username = 'D00194632';
+    $password = 'cPuZDVy^';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+?>
