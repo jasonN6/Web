16be8e21afb75c771388946779205867a228f624
D00194632 <D00194632@P1105-PC15.ad.dkit.ie>
2019-03-02 17:33:19 +0000

    Commit 18: Adds the database credentials to connect to the server in comments on the database.php page.

--- model/database.php
+++ model/database.php
@@ -2,6 +2,10 @@
     $dsn = 'mysql:host=localhost;dbname=D00194632';
     $username = 'root';
     $password = '';
+    
+//     $dsn = 'mysql:host=localhost;dbname=D00194632';
+//     $username = 'D00194632';
+//    $password = 'cPuZDVy^';
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
